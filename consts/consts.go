bbc5ca298052f877907e903aee42494341230f02
https-kintsugi <145455899+https-kintsugi@users.noreply.github.com>
2023-11-19 23:22:56 +0800

    Initial commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "gelo"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "PlantVsEnemy"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PVE"
 )
 
 var ID ids.ID
